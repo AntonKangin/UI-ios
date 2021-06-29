e811193b7d3ff4752ce7b8f3de72c04a1a5d05fa
Anton Kangin <Anton.kangin@gmail.com>
2021-06-29 20:22:51 +0400

    add profile files, change background color to lightGray

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Антон Кангин on 29.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+}
---
+++ Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Антон Кангин on 29.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+      view.backgroundColor = .lightGray
+    }
+}
